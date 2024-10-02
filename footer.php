43719d6d82d13d6ef886f509bc4e51d03119dab0
gaurav-sharma-ui <41190894+gaurav-sharma-ui@users.noreply.github.com>
2024-10-03 00:47:58 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer>
+  <div class="conatainer">
+    <div class="row">
+      <div class="col-8">
+        
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      
+    </div>
+  </div>
+</footer>
+</body>
+</html>
